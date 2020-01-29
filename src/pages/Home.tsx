00f500ae6e94dde0e16e99a96ac013fc50ebc03f
Teerasej <teerasej@gmail.com>
2020-01-29 22:04:28 +0700

    first commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import React from 'react';
+import { Plugins } from '@capacitor/core';
+const { Geolocation } = Plugins;
 
 const Home: React.FC = () => {
   return (
