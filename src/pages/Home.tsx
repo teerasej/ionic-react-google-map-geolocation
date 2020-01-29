8006a2a3012f95f5c31605e066e25cb6a03f56f1
Teerasej <teerasej@gmail.com>
2020-01-29 22:16:13 +0700

    show loading when start getGeolocation

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,24 +1,42 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon } from '@ionic/react';
-import React from 'react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonButton, IonIcon, IonLoading } from '@ionic/react';
+import React, { useState } from 'react';
 import { locate } from "ionicons/icons";
 import { Plugins } from '@capacitor/core';
 const { Geolocation } = Plugins;
 
 const Home: React.FC = () => {
+
+  const [showLoading, setshowLoading] = useState(false);
+
+  const startGetGPS = async () => {
+    console.log('getting gps');
+    setshowLoading(true);
+    const coordinates = await Geolocation.getCurrentPosition();
+    setshowLoading(false);
+    console.log('Current', coordinates);
+  }
+
+
   return (
     <IonPage>
       <IonHeader>
         <IonToolbar>
           <IonTitle>My Location</IonTitle>
           <IonButtons slot="end">
-            <IonButton>
+            <IonButton
+              onClick={startGetGPS}
+            >
               <IonIcon icon={locate} slot="icon-only"></IonIcon>
             </IonButton>
           </IonButtons>
         </IonToolbar>
       </IonHeader>
       <IonContent className="ion-padding">
-        
+      <IonLoading
+        isOpen={showLoading}
+        message={'Getting GPS...'}
+        duration={5000}
+      />
       </IonContent>
     </IonPage>
   );
