324a026fa56e8ce76d316a60782f35ed64867e09
Teerasej <teerasej@gmail.com>
2020-01-31 11:44:26 +0700

    complete

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,11 +2,35 @@ import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonButtons, IonBu
 import React, { useState } from 'react';
 import { locate } from "ionicons/icons";
 
+import { Plugins } from "@capacitor/core";
+const { Geolocation } = Plugins;
 
 const Home: React.FC = () => {
 
+  const [showLoading, setShowLoading] = useState(false);
+  const [positionReady, setPositionReady] = useState(false);
+  const [currentPosition, setCurrentPosition] = useState({lat:0, lng:0, acc:0});
+
   const startGetGPS = async () => {
-  
+    console.log('Getting geolocation...');
+    setShowLoading(true);
+    let coordinates = await Geolocation.getCurrentPosition();
+    setShowLoading(false);
+    setPositionReady(true);
+    setCurrentPosition({
+      lat: coordinates.coords.latitude,
+      lng: coordinates.coords.longitude,
+      acc: coordinates.coords.accuracy
+    });
+    console.log('Current: ', coordinates);
+  }
+
+  let positionTextJSX;
+
+  if(positionReady == true) {
+    positionTextJSX = <p>{currentPosition.lat}, {currentPosition.lng}, {currentPosition.acc}</p>
+  } else {
+    positionTextJSX = <p>กดปุ่มบนขวาเพื่อระบุพิกัดของเครื่อง</p>
   }
 
 
@@ -25,7 +49,8 @@ const Home: React.FC = () => {
         </IonToolbar>
       </IonHeader>
       <IonContent>
-        
+        {positionTextJSX}
+        <IonLoading isOpen={showLoading} message="กำลังขอพิกัด"/>
       </IonContent>
     </IonPage>
   );
